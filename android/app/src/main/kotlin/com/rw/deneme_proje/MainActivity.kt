4b0c6e63ac519cf1afeb92e6366b981d08f15b16
Mahmut ALSAMOUR <mahmut220509601@gmail.com>
2025-09-22 09:37:53 +0300

    Initial cleaned commit — remove build outputs

---
+++ android/app/src/main/kotlin/com/rw/deneme_proje/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rw.deneme_proje
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+  void _incrementCounter() {
+    _counter++;
+    setState(() {});
+  }
+
+  void _resetCounter() {
+    _counter = 0;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF191919),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset("assets/images/zikirmatik.png", width: 300),
+                Positioned(top: 50, right: 80, child: _counterText()),
+                Positioned(bottom: 30, child: _incrementButton()),
+                Positioned(bottom: 114, right: 76, child: _resetButton()),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  GestureDetector _resetButton() {
+    return GestureDetector(
+      onTap: _resetCounter,
+      child: Container(
+        width: 30,
+        height: 30,
+
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(60)),
+      ),
+    );
+  }
+
+  GestureDetector _incrementButton() {
+    return GestureDetector(
+      onTap: _incrementCounter,
+      child: Container(
+        width: 90,
+        height: 90,
+
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(60)),
+      ),
+    );
+  }
+
+  Text _counterText() {
+    return Text(
+      '$_counter',
+      style: TextStyle(
+        fontFamily: "Digital7",
+        fontSize: 50,
+        color: Colors.white,
+      ),
+    );
+  }
+}
